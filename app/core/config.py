a38a68480304122c2b83489c4f6af764796f1c58
Damassdev <damassdev@gmail.com>
2026-05-23 23:19:23 +0700

    deploy: render

--- app/core/config.py
+++ app/core/config.py
@@ -2,32 +2,42 @@
 <> KONFIGURASI aplikasi terpusat <>
 """
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+load_dotenv()
+
+
 class Settings(BaseSettings):
+
     # <> Info Aplikasi <>
-    APP_NAME: str = "Nutrify AI - API"
-    APP_VERSION: str = "1.0"
-    APP_DESCRIPTION: str = "Indonesian Food Recognation and Recomendation For Disease"
-    
-    MODEL_PATH: str = "nutrify_model.keras"
-    CLASS_NAMES_PATH: str = "class_names.json"
-    DATASETS: str = "indonesian_food_clean.csv"
-    
-    IMG_SIZE: int = 224
-    
-    REJECT_THRESHOLD: float = 0.61
-    WARN_THRESHOLD: float = 0.62
-    
-    TEMPERATURE_SCALE: float = 5.0
-    
-    CORS: str = "*"
-    
-    HOST: str = "0.0.0.0"
-    PORT: int = 8173
-    
-    LOG_LEVEL: str = "INFO"
-    
+    APP_NAME: str
+    APP_VERSION: str
+    APP_DESCRIPTION: str
+
+    # <> File Path <>
+    MODEL_PATH: str
+    CLASS_NAMES_PATH: str
+    DATASETS: str
+
+    # <> CNN Config <>
+    IMG_SIZE: int
+
+    REJECT_THRESHOLD: float
+    WARN_THRESHOLD: float
+
+    TEMPERATURE_SCALE: float
+
+    # <> CORS <>
+    CORS: str
+
+    # <> Server <>
+    HOST: str
+    PORT: int
+
+    # <> Logging <>
+    LOG_LEVEL: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
@@ -36,9 +46,15 @@ class Settings(BaseSettings):
 
     @property
     def cors_origins_list(self) -> list[str]:
+
         if self.CORS.strip() == "*":
             return ["*"]
 
-        return [o.strip() for o in self.CORS.split(",") if o.strip()]
+        return [
+            origin.strip()
+            for origin in self.CORS.split(",")
+            if origin.strip()
+        ]
+
 
 settings = Settings()
